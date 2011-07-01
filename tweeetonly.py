afbb83a74b8b7b23eec0d04c6f01bc3fad16b117
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-07-01 13:13:07 +0900

    キーを設定ファイルに分けた

---
+++ tweeetonly.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from twython import Twython
+import sys
+from ConfigParser import SafeConfigParser as ConfigParser
+
+# read config
+config = ConfigParser()
+config.read('.tweeetonly')
+CONSUMER_KEY        = config.get('tweeetonly', 'consumer_key')
+CONSUMER_SECRET     = config.get('tweeetonly', 'consumer_secret')
+ACCESS_TOKEN        = config.get('tweeetonly', 'access_token')
+ACCESS_TOKEN_SECRET = config.get('tweeetonly', 'access_token_secret')
+
+argvs = sys.argv
+text = argvs[1]
+
+tw = Twython(
+    twitter_token = CONSUMER_KEY,
+    twitter_secret = CONSUMER_SECRET,
+    oauth_token = ACCESS_TOKEN,
+    oauth_token_secret = ACCESS_TOKEN_SECRET)
+tw.updateStatus(status = text)
